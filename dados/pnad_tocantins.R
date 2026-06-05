39b09bdb931fd3ee53722474385e3a97538a9903
Christian Martins <martinschristiandavid@gmail.com>
2026-06-05 11:13:03 -0300

    Rename variable from dados_recortados to dados_to

--- dados/pnad_tocantins.R
+++ dados/pnad_tocantins.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 pnad_completa <- get_pnadc(2019, 4, design = FALSE)
 
-dados_recortados <- pnad_completa |> 
+dados_to <- pnad_completa |> 
   filter(
     UF == "Tocantins",       
     !is.na(VD4016),          
@@ -13,4 +13,4 @@ dados_recortados <- pnad_completa |>
   select(UF, UPA, V1028, V2007, V2009, VD4016)
 
 dir.create("dados", showWarnings = FALSE)
-write_csv(dados_recortados, "tocantins.csv")
+write_csv(dados_to, "tocantins.csv")
